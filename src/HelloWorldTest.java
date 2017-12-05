3300a1fa7cfd9f5d8484b4d03729677309232a05
ofirgut007 <ofirgut007@gmail.com>
2017-12-05 23:02:53 +0200

    Added junit

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,10 @@
+c class HelloWorldTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+        fail("O.M.G. - this is an error !");
+    }
+    
+}
