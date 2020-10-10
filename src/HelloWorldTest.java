3f5f4011f09f62e55818fb6738968af65020773f
Ofir Gutmacher <ofirgu@boltinc.com>
2020-10-10 11:55:05 +0300

    commit with c

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -4,7 +4,7 @@ public class HelloWorldTest extends junit.framework.TestCase {
     }
     
     public void testWillAlwaysFail() {
-        System.out.println("this is an errora");
+        System.out.println("this is an errorc");
     }
     
 }
